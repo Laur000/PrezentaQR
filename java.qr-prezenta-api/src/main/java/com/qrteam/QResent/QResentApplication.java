f8f9d8cfa4d25e0d19842029d78e1cd1d6a3ff62
SanduEduard <sandueduard89@gmail.com>
2021-10-28 17:55:11 +0300

    added configuration

--- java.qr-prezenta-api/src/main/java/com/qrteam/QResent/QResentApplication.java
+++ java.qr-prezenta-api/src/main/java/com/qrteam/QResent/QResentApplication.java
@@ -2,7 +2,9 @@ package com.qrteam.QResent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class QResentApplication {
 
