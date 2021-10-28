4fc3756f0f0135260059a5a67ee2d01c3afc83ff
SanduEduard <sandueduard89@gmail.com>
2021-10-28 16:43:40 +0300

    init api commit

---
+++ java.qr-prezenta-api/src/main/java/com/qrteam/QResent/QResentApplication.java
@@ -0,0 +1,13 @@
+package com.qrteam.QResent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QResentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QResentApplication.class, args);
+	}
+
+}
---
+++ java.qr-prezenta-api/src/test/java/com/qrteam/QResent/QResentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrteam.QResent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QResentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
