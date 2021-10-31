9cea0ea2e71d070f1bb8b56277ec52fd4e773579
SanduEduard <sandueduard89@gmail.com>
2021-10-31 15:05:40 +0200

    request test

--- java.qr-prezenta-api/src/main/java/com/qrteam/QResent/app/QResentApplication.java
+++ java.qr-prezenta-api/src/main/java/com/qrteam/QResent/app/QResentApplication.java
@@ -3,11 +3,6 @@ package com.qrteam.QResent.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.qrteam")
--- java.qr-prezenta-api/src/main/java/com/qrteam/QResent/controller/PrezentaQRController.java
+++ java.qr-prezenta-api/src/main/java/com/qrteam/QResent/controller/PrezentaQRController.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class PrezentaQRController {
@@ -15,7 +18,7 @@ public class PrezentaQRController {
     @Autowired
     PrezentaQRChoreographer prezentaQRChoreographer;
 
-    @PostMapping("/get-student-data")
+    @RequestMapping(value = "/get-student-data", method = POST, produces = "application/json")
     ResponseEntity<String> getStudentData(@RequestBody int id) {
         return new ResponseEntity<>(prezentaQRChoreographer.getStudentData(id), HttpStatus.OK);
     }
