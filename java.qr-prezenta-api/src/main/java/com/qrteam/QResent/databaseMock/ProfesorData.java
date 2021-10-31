5e868148b035ca425e7cd296a5953230f0b7191c
Andrei Diaconu <adiaconu55@gmail.com>
2021-10-31 16:02:15 +0200

    Fixed getStudentData Service method

--- java.qr-prezenta-api/src/main/java/com/qrteam/QResent/databaseMock/ProfesorData.java
+++ java.qr-prezenta-api/src/main/java/com/qrteam/QResent/databaseMock/ProfesorData.java
@@ -1,9 +1,11 @@
 package com.qrteam.QResent.databaseMock;
 import com.qrteam.QResent.models.Profesor;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class ProfesorData {
     private List<Profesor> profesori;
 
