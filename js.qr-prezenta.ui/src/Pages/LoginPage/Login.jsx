2a27b10e84f8346768cf121997a7991c124882b2
Vali <toeavalidaniel@gmail.com>
2021-10-31 14:48:39 +0200

    login page

--- js.qr-prezenta.ui/src/Pages/LoginPage/Login.jsx
+++ js.qr-prezenta.ui/src/Pages/LoginPage/Login.jsx
@@ -2,12 +2,14 @@ import LoginForm from "../../Components/LoginForm/Loginform";
 import styles from "./Login.module.css";
 import React from "react";
 const Login = () => {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}> QR Attendance </h1>
-            <LoginForm className={styles.wrapper}/>
-        </div>
-    );
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}> QR Attendance </h1>
+        <LoginForm className={styles.loginForm} />
+      </div>
+    </div>
+  );
+};
 
 export default Login;
