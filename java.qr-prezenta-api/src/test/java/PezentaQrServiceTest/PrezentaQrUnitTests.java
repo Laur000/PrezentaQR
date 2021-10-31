59aa724195a4b519bd02b0beffac1c2f36779cc4
Laurentiu Bagiu <laurlaurbagiu1.0@yahoo.com>
2021-10-31 16:37:54 +0200

    test

--- java.qr-prezenta-api/src/test/java/PezentaQrServiceTest/PrezentaQrUnitTests.java
+++ java.qr-prezenta-api/src/test/java/PezentaQrServiceTest/PrezentaQrUnitTests.java
@@ -1,13 +1,33 @@
 package PezentaQrServiceTest;
 
+import com.qrteam.QResent.databaseMock.StudentData;
+import com.qrteam.QResent.dto.LoginRequestDTO;
+import com.qrteam.QResent.dto.StudentDTO;
+import com.qrteam.QResent.service.Impl.PrezentaQRServiceImpl;
+import com.qrteam.QResent.service.PrezentaQRService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@ExtendWith(MockitoExtension.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class PrezentaQrUnitTests {
+    @Autowired
+    StudentData studentData;
+    @Autowired
+    PrezentaQRService prezentaQRService;
+
     @Test
     public void shouldReturnStudentData() {
-        System.out.println("Muie la unguri");
+        LoginRequestDTO loginRequest = new LoginRequestDTO();
+        loginRequest.setEmail("emailpentrufacultatefake1@facultate.stud.ro");
+        loginRequest.setPassword("parolafake1");
+
+        StudentDTO studentDTO = prezentaQRService.getStudentData(loginRequest);
+        assertEquals("Andrei", studentDTO.getFirstName());
     }
 }
