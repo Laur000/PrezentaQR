5826195786ca3bb0900bdf4ff2c4b40c36098013
Sergiu Voicu <sergiu.ciprian.voicu@gmail.com>
2021-10-31 19:40:03 +0200

    logout icon

--- js.qr-prezenta.ui/src/Pages/Dashboard/HeaderContent/HeaderContent.jsx
+++ js.qr-prezenta.ui/src/Pages/Dashboard/HeaderContent/HeaderContent.jsx
@@ -1,39 +1,39 @@
 import React from "react";
 import { UserContext } from "../../../context/UserContext";
-import { Avatar, ThemeIcon, Menu } from "@mantine/core";
+import { Avatar, ThemeIcon, Menu, Container } from "@mantine/core";
 import styles from "./HeaderContent.module.css";
-import { ExitIcon, LightningBoltIcon} from "@radix-ui/react-icons";
+import { ExitIcon, LightningBoltIcon } from "@radix-ui/react-icons";
 
 const HeaderContent = () => {
-  const [user, setUser] = React.useContext(UserContext);
+    const [user, setUser] = React.useContext(UserContext);
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        <ThemeIcon
-          size="lg"
-          radius="lg"
-          variant="gradient"
-          gradient={{ from: "teal", to: "grape" }}
-        >
-          <LightningBoltIcon />
-        </ThemeIcon>
-        <span className={styles.title}>QResent</span>
-      </div>
-      <div className={styles.user}>
-        <span>{user && user.username}</span>
-        <Menu control={<Avatar radius="lg" size="md" />}>
-          <Menu.Item
-            component="button"
-            icon={<ExitIcon />}
-            onClick={() => setUser(null)}
-          >
-            Sign Out
-          </Menu.Item>
-        </Menu>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                <ThemeIcon
+                    size="lg"
+                    radius="lg"
+                    variant="gradient"
+                    gradient={{ from: "teal", to: "grape" }}
+                >
+                    <LightningBoltIcon />
+                </ThemeIcon>
+                <span className={styles.title}>QResent</span>
+            </div>
+
+
+            <Menu control={<div className={styles.user}>{user && user.username}<Avatar radius="lg" size="md" /> </div>}>
+                <Menu.Item
+                    component="button"
+                    icon={<ExitIcon />}
+                    onClick={() => setUser(null)}
+                >
+                    Sign Out
+                </Menu.Item>
+            </Menu>
+
+        </div>
+    );
 };
 
 export default HeaderContent;
