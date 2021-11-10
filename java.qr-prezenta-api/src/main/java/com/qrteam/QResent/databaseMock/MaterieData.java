218cf464ccda35b3fac3dfc943ad0cf785dcefe5
Laurentiu Bagiu <laurlaurbagiu1.0@yahoo.com>
2021-11-10 19:53:24 +0200

    MaterieData details updated.

--- java.qr-prezenta-api/src/main/java/com/qrteam/QResent/databaseMock/MaterieData.java
+++ java.qr-prezenta-api/src/main/java/com/qrteam/QResent/databaseMock/MaterieData.java
@@ -15,18 +15,18 @@ public class MaterieData {
     public void init(){
         materii = new ArrayList<>();
 
-        materii.add(new Materie(1, "Anatomie",""));
-        materii.add(new Materie(2, "Geografie",""));
-        materii.add(new Materie(3, "Managementul Proiectelor",""));
-        materii.add(new Materie(4, "Istorie",""));
-        materii.add(new Materie(5, "Mate1",""));
-        materii.add(new Materie(6, "Mate2",""));
-        materii.add(new Materie(7, "Mate3",""));
-        materii.add(new Materie(8, "Fizica",""));
-        materii.add(new Materie(9, "Fizica2",""));
-        materii.add(new Materie(10, "Electrotehnica",""));
-        materii.add(new Materie(11, "Electromagnetica",""));
-        materii.add(new Materie(12, "Chimie",""));
+        materii.add(new Materie(1, "Anatomie","Frumoasa materia\nCerinte minime de intrare in examen: 50pct\nBonusuri: n-avem\nOrar: LUNI 18:00-20:00\n JOI 12:00-15:00"));
+        materii.add(new Materie(2, "Geografie","Frumoasa materia\nCerinte minime de intrare in examen: 45pct\nBonusuri:2\nOrar: MARTI 18:00-20:00\n JOI 10:00-12:00"));
+        materii.add(new Materie(3, "Managementul Proiectelor","Frumoasa materia\nCerinte minime de intrare in examen: 20pct\nBonusuri: n-avem\nOrar: LUNI 11:00-16:00"));
+        materii.add(new Materie(4, "Istorie","Frumoasa materia\nCerinte minime de intrare in examen: 50pct\nBonusuri: 12pct\nOrar: MIERCURI 11:00-16:00"));
+        materii.add(new Materie(5, "Mate1","Frumoasa materia\nCerinte minime de intrare in examen: 20pct\nBonusuri: n-avem\nOrar: VINERI 18:00-20:00"));
+        materii.add(new Materie(6, "Mate2","Frumoasa materia\nCerinte minime de intrare in examen: 60pct\nBonusuri: n-avem\nOrar: LUNI 09:00-12:00"));
+        materii.add(new Materie(7, "Mate3","Frumoasa materia\nCerinte minime de intrare in examen: 30pct\nBonusuri: 5pct\nOrar: MIERCURI 08:00-11:00"));
+        materii.add(new Materie(8, "Fizica","Frumoasa materia\nCerinte minime de intrare in examen: 50pct\nBonusuri: n-avem\nOrar: MARTI 08:00-10:00"));
+        materii.add(new Materie(9, "Fizica2","Frumoasa materia\nCerinte minime de intrare in examen: 20pct\nBonusuri: n-avem\nOrar: LUNI 16:00-18:00"));
+        materii.add(new Materie(10, "Electrotehnica","Frumoasa materia\nCerinte minime de intrare in examen: 10pct\nBonusuri: 10pct\nOrar: VINERI 11:00-16:00"));
+        materii.add(new Materie(11, "Electromagnetica","Frumoasa materia\nCerinte minime de intrare in examen: 20pct\nBonusuri: n-avem\nOrar: VINERI 08:00-11:00"));
+        materii.add(new Materie(12, "Chimie","Frumoasa materia\nCerinte minime de intrare in examen: 70pct\nBonusuri: n-avem\nOrar: JOI 07:00-10:00"));
 
     }
 
