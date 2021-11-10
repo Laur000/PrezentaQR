4d2e6a772b93079032f17ba0dc8a5b534851dcbc
Bogdan Zat <zatbogdan99@gmail.com>
2021-11-10 19:14:16 +0200

    added MaterieDTO

---
+++ java.qr-prezenta-api/src/main/java/com/qrteam/QResent/dto/MaterieDTO.java
@@ -0,0 +1,35 @@
+package com.qrteam.QResent.dto;
+
+import com.qrteam.QResent.models.Curs;
+
+import java.util.List;
+
+public class MaterieDTO {
+    private Integer id;
+    private String nume;
+    private List<Curs> cursuri;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public List<Curs> getCursuri() {
+        return cursuri;
+    }
+
+    public void setCursuri(List<Curs> cursuri) {
+        this.cursuri = cursuri;
+    }
+}
