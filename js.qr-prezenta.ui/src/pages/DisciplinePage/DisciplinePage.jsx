13714358d4dc8f2033592d3b8474fec09c60857c
Vali <toeavalidaniel@gmail.com>
2021-11-18 22:36:23 +0200

    api fails

--- js.qr-prezenta.ui/src/pages/DisciplinePage/DisciplinePage.jsx
+++ js.qr-prezenta.ui/src/pages/DisciplinePage/DisciplinePage.jsx
@@ -2,17 +2,27 @@ import { Button } from "@mantine/core";
 import React from "react";
 import { ArrowLeftIcon } from "@radix-ui/react-icons";
 import { useHistory } from "react-router";
+import axios from "axios";
+import { MAIN_URL } from "../../utils/url";
+import { useSelector } from "react-redux";
 
 const DisciplinePage = (props) => {
   const params = props.match.params;
   const history = useHistory();
+  const userEmail = useSelector((state) => state.user.userData.email);
+
+  React.useEffect(() => {
+    axios
+      .post(MAIN_URL + "get-prof-disciplines", { email: userEmail })
+      .then((response) => console.log(response.data));
+  }, []);
 
   return (
     <div>
       <Button
         leftIcon={<ArrowLeftIcon />}
         variant="white"
-        style={{ background: "transparent", padding: 0}}
+        style={{ background: "transparent", padding: 0 }}
         onClick={() => history.goBack()}
       >
         Back to Disciplines
