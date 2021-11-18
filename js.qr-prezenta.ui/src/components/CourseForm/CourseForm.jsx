18e07f0628655833ee18fbc06ee47d717830542f
Sergiu Voicu <sergiu.ciprian.voicu@gmail.com>
2021-11-18 22:14:12 +0200

    courses page

---
+++ js.qr-prezenta.ui/src/components/CourseForm/CourseForm.jsx
@@ -0,0 +1,80 @@
+import { Button, Input, TextInput } from "@mantine/core";
+import { useNotifications } from "@mantine/notifications";
+import axios from "axios";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { MAIN_URL } from "../../utils/url";
+import styles from "./CourseForm.module.css";
+
+const CourseForm = (props) => {
+  let ENDPOINT = "save-course";
+  const notifications = useNotifications();
+  const user = useSelector((state) => state.user.userData);
+  const [newCourse, setNewCourse] = useState({
+    disciplineId: 0, cursDTO:{cursId: 0}
+  });
+
+  const saveNewCourse = async () => {
+    await axios
+      .post(MAIN_URL + ENDPOINT, {
+        ...newCourse,
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        if (data) {
+          notifications.showNotification({
+            title: "Submit new course",
+            message: "Course successfully added",
+            color: "green",
+          });
+          props.closeModal();
+        } else {
+          notifications.showNotification({
+            title: "Submit new course",
+            message: "Something went wrong while adding the new course 🤥",
+            color: "red",
+          });
+        }
+      });
+  };
+  return (
+    <div className={styles.container}>
+      <TextInput
+        className={styles.field}
+        placeholder="Course name"
+        label="Course name"
+        onChange={(event) => {
+          setNewCourse((prev) => ({
+            ...prev,
+            cursDTO: { ...prev.cursDTO, nume: event.target.value },
+          }));
+        }}
+        required
+      />
+
+      <TextInput
+        className={styles.field}
+        placeholder="Details"
+        label="Details"
+        onChange={(event) => {
+          setNewCourse((prev) => ({
+            ...prev,
+            cursDTO: { ...prev.cursDTO, detalii: event.target.value },
+          }));
+        }}
+      />
+
+      <Button
+        className={styles.button}
+        color="teal"
+        onClick={() => {
+          saveNewCourse();
+        }}
+      >
+        Save
+      </Button>
+    </div>
+  );
+};
+
+export default CourseForm;
--- js.qr-prezenta.ui/src/pages/DisciplinePage/DisciplinePage.jsx
+++ js.qr-prezenta.ui/src/pages/DisciplinePage/DisciplinePage.jsx
@@ -1,23 +1,69 @@
-import { Button } from "@mantine/core";
-import React from "react";
-import { ArrowLeftIcon } from "@radix-ui/react-icons";
+import { Button, Card, Group, List, Text, ThemeIcon, Modal } from "@mantine/core";
+import React, { useState } from "react";
+import { ArrowLeftIcon, CheckCircledIcon } from "@radix-ui/react-icons";
 import { useHistory } from "react-router";
+import CourseForm from "../../components/CourseForm/CourseForm";
+import styles from "./DisciplinePage.module.css";
 
 const DisciplinePage = (props) => {
   const params = props.match.params;
   const history = useHistory();
-
+  const [modalState, setModalState] = useState(false);
+  const mock = [
+    { name: "Cursu unu", description: "Fresh nu-i bine deloc" },
+    { name: "Cursul 2PM", description: "Salutare mos craciun" },
+    {
+      name: "Curva 3",
+      description: "Sa ma arunc de pe camin sau sa ma arunc de pe camin",
+    },
+  ];
   return (
     <div>
       <Button
         leftIcon={<ArrowLeftIcon />}
         variant="white"
-        style={{ background: "transparent", padding: 0}}
+        style={{ background: "transparent", padding: 0 }}
         onClick={() => history.goBack()}
       >
         Back to Disciplines
       </Button>
-      <h1>{params.id}</h1>
+      <div className={styles.buttonContainer}>
+        <h1>{params.id}</h1>
+        <Button
+          variant="gradient"
+          gradient={{ from: "teal", to: "blue", deg: 60 }}
+          onClick={() => {
+            setModalState(true);
+          }}
+        >
+          Add Course
+        </Button>
+      </div>
+      <List spacing="md" listStyleType="none">
+        {mock.map((obj, idx) => (
+          <List.Item>
+            <Card className={styles.courseContainer} shadow="sm" radius="md">
+              <ThemeIcon color="teal" size={48} radius="xl">
+                <CheckCircledIcon style={{ width: 48, height: 48 }} />
+              </ThemeIcon>
+              <Group className={styles.course}>
+                <Text className={styles.title}>{obj.name}</Text>
+                <Text className={styles.description}>{obj.description}</Text>
+              </Group>
+            </Card>
+          </List.Item>
+        ))}
+      </List>
+      <Modal
+        opened={modalState}
+        onClose={() => {
+          setModalState(false);
+        }}
+        title="New Course"
+      >
+        <CourseForm closeModal={() => setModalState(false)} />
+      </Modal>
+      
     </div>
   );
 };
